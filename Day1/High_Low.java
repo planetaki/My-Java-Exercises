d6059ee82a4a5703037c8c2b8aa15e92dbc9e066
Anastasia Fliostor <fliostor.anastasia@gmail.com>
2024-10-30 12:04:29 +0000

    division exercise+comments

--- Day1/High_Low.java
+++ Day1/High_Low.java
@@ -1,15 +1,12 @@
-/**Read an arbitrarily long sequence of positive numbers from the user, until -1 is entered. At that point,
- print “Yes” if the numbers were consecutive and increasing and “No” otherwise. For example, the
- sequences “1,2,3,4,-1” and “5,6,7,8,9,10,11,-1” should lead to the output “Yes”, but “2,3,5,6,7,-1”,
- “10,9,8,7,-1”, and “1,1,2,3,4,5,-1” should lead to the output ”No”.
- **/
+/**Modify your program from the previous exercise so that it outputs “Yes” when the numbers are consecutive, regardless of whether they go up or down. For example, both “2,3,4,5,6,-1” and “10,9,8,7,-
+ 1” should now result in “Yes”.**/
 
 package src;
 
 import java.util.Scanner;
 
-public class GoingUp {
-    public GoingUp() {
+public class High_Low {
+    public High_Low() {
     }
 
     public static void main(String[] args) {
@@ -26,13 +23,14 @@ public class GoingUp {
                 break;
             }
 
-            if (input >= previous) {
-                previous = input;
-            } else {
+            if (input != previous + 1 && input != previous - 1) {
                 result = "no";
+            } else {
+                previous = input;
             }
         }
 
         System.out.print(result);
     }
 }
+
